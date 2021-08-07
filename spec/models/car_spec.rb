b4f42e761ed2adca58fe5fae1d66727980198cd8
Steven Sim <stevensim226@gmail.com>
2021-08-07 21:05:02 +0700

    Test fail test

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Car, type: :model do
     end
 
     it 'ensure name getter' do
-      expect(@c1.name).to eq("Car One")
+      expect(@c1.name).to eq("Car Onee")
     end
 
     it 'ensure license getter' do
